df7a34bf03f32019750cb3170e298e62897a8204
Serv08 <i.am.servinsanchez@gmail.com>
2024-11-24 17:39:47 +0800

    add skills in home page

--- js/script.js
+++ js/script.js
@@ -26,3 +26,24 @@ filterButtons.forEach(button => {
     }
   });
 });
+
+
+document.querySelectorAll('.skills-box a').forEach(item => {
+  item.addEventListener('click', function(e) {
+    e.preventDefault(); // Prevent default anchor behavior
+
+    // Close all categories first
+    const allCategories = document.querySelectorAll('.skills-category');
+    allCategories.forEach(category => {
+      if (category !== this.closest('.skills-category')) {
+        category.classList.remove('active');
+      }
+    });
+
+    // Toggle the active state for the clicked category
+    const category = this.closest('.skills-category');
+    category.classList.toggle('active');
+  });
+});
+
+

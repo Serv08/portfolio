bc3647353c08caad7c313d791860411ed78df703
Serv08 <i.am.servinsanchez@gmail.com>
2024-11-27 23:26:19 +0800

    fix: js function on the wrong file

--- js/project-script.js
+++ js/project-script.js
@@ -28,25 +28,6 @@ filterButtons.forEach(button => {
 });
 
 
-document.querySelectorAll('.skills-box a').forEach(item => {
-  item.addEventListener('click', function(e) {
-    e.preventDefault(); // Prevent default anchor behavior
-
-    // Close all categories first
-    const allCategories = document.querySelectorAll('.skills-category');
-    allCategories.forEach(category => {
-      if (category !== this.closest('.skills-category')) {
-        category.classList.remove('active');
-      }
-    });
-
-    // Toggle the active state for the clicked category
-    const category = this.closest('.skills-category');
-    category.classList.toggle('active');
-  });
-});
-
-
 // Search function
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("search-input").addEventListener("input", function () {
